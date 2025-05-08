1b63dc3dce03a3afc2c5ff69f80b761a7b476d24
Daniela Luhrsen <daniela.luhrsen@bsc.es>
2025-05-08 10:46:27 +0200

    added fucntion1 and documented

--- R/function1.R
+++ R/function1.R
@@ -1,3 +1,11 @@
+#' Add two numbers
+#'
+#' This function adds two numbers together.
+#'
+#' @param x A numeric value.
+#' @return The sum of `x` and `y`.
+#' @export
+#' 
 function1 <- function(x) {
     print(paste0(" you passed parameter ", x))
 }
